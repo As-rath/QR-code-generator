c2aab54e693037b13fed6784475fb554987f7084
As-rath <asrath.015@gmail.com>
2025-12-21 23:06:59 +0530

    Cleared up the code

---
+++ GUI.py
@@ -0,0 +1,47 @@
+from main import generateQrCode, browseFile, saveQrCode, onGenerate
+from PIL import Image, ImageTk
+import tkinter as tk
+
+#Creating the main window (GUI)
+root = tk.Tk()
+root.title("QR Code Generator")
+root.geometry("800x600")
+root.resizable(True, True)
+
+#Data input
+data_frame = tk.Frame(root)
+data_frame.pack(pady = (20, 5))
+tk.Label(data_frame, text="Data/URL:").pack(pady = (20, 5))
+data_entry = tk.Entry(data_frame, width = 50)
+data_entry.pack()
+
+#Logo input
+logo_frame = tk.Frame(root)
+logo_frame.pack(pady = (20, 5))
+tk.Label(logo_frame, text="Logo Image:").pack(pady = (20, 5))
+logo_entry = tk.Entry(logo_frame, width = 50)
+logo_entry.pack()
+tk.Button(logo_frame, text="Browse", command = browseFile).pack(pady = (10, 0), side = tk.LEFT)
+
+#Save location input
+# save_frame = tk.Frame(root)
+# save_frame.pack(pady = (20, 5))
+# tk.Label(save_frame, text = "Save QR Code as: ").pack(pady = (10, 5))
+# save_entry = tk.Entry(save_frame, width = 50)
+# save_entry.pack()
+# tk.Button(save_frame, text = "Save As", command = browseSaveLocation).pack(pady = (10, 0), side = tk.LEFT)
+
+
+#Generate Button
+tk.Button(root, text = "Generate QR Code", command = onGenerate , bg = "#000000", fg = "white", width = 20).pack(pady = 20)
+
+#Preview of the generated QR
+preview_frame = tk.Frame(root)
+preview_frame.pack(pady = 10)
+preview_label = tk.Label(preview_frame, text = "QR Code Preview will appear here")
+preview_label.pack(pady = (10, 5))
+
+#Save Button
+tk.Button(root, text = "Save QR Code", command = saveQrCode , bg = "#000000", fg = "white", width = 20).pack(pady = 10)
+
+root.mainloop()
