f54b449bd960876f2e22c985bb4f68ce57bfe557
As-rath <asrath.015@gmail.com>
2025-12-21 11:31:37 +0530

    Modified the logo adding feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import qrcode
 from qrcode.constants import ERROR_CORRECT_H
-from PIL import Image
+from PIL import Image, ImageDraw
 
 def generate_qr_code(data, filename):
     # Creating a QR code instance 
@@ -16,7 +16,7 @@ def generate_qr_code(data, filename):
     qr.make(fit = True)
 
     # Creating an image from the QR Code instance
-    img = qr.make_image(fill_color = "blue", back_color = "white").convert("RGBA")
+    img = qr.make_image(fill_color = "black", back_color = "white").convert("RGBA")
 
     #Opening the logo image
     logo_path = input("Enter the path to the logo image (e.g., logo.webp): ")
@@ -27,10 +27,27 @@ def generate_qr_code(data, filename):
     logo_size = qr_width // 5
     logo = logo.resize((logo_size, logo_size), Image.LANCZOS)
 
+    #Buffer 
+    padding = 12
+    bg_size = logo_size + padding*2 
+
+    #Creating a background for the logo
+    logo_bg = Image.new("RGBA", (bg_size, bg_size), (255, 255, 255, 255))
+    mask = Image.new("L", (bg_size, bg_size), 0)
+
+    #Drawing a rounded rectangle as mask
+    draw = ImageDraw.Draw(mask)
+    radius = bg_size // 6
+    draw.rounded_rectangle((0, 0, bg_size, bg_size), radius, fill=255)
+
+    logo_bg.putalpha(mask)  
+
     #Calculating the position to paste the logo
-    logo_pos = ((qr_width - logo_size) //2, (qr_height - logo_size) //2)
+    bg_pos = ((qr_width - logo_size) //2, (qr_height - logo_size) //2)
+    logo_pos = (bg_pos[0] + padding, bg_pos[1] + padding)
 
     #Pasting the logo onto the QR code
+    img.paste(logo_bg, bg_pos, logo_bg)
     img.paste(logo, logo_pos, logo)
     
     # Saving the image to a file
