7928b3aad2397897fcc91a9746205d894264e1ee
As-rath <asrath.015@gmail.com>
2025-12-22 14:45:35 +0530

    Completed with the web app.

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
 from main import * 
 from flask import Flask, render_template, request, send_file
 import os
+import uuid
 
 app = Flask(__name__)
 
-OUTPUT_DIR = "static/outputs"
+OUTPUT_DIR = "QR-code-generator/static/outputs"
 os.makedirs(OUTPUT_DIR, exist_ok = True)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
+    qr_image_URL = None
+
     if request.method == "POST":
         data = request.form["data"]
         logo = request.files["logo"]
 
-        logo_path = os.path.join(OUTPUT_DIR, logo.filename)
+        #Save Uploaded Logo
+        logo_filename = f"{uuid.uuid4()}_{logo.filename}"
+        logo_path = os.path.join(OUTPUT_DIR, logo_filename)
         logo.save(logo_path)
 
+        #Generate QR Code
         img = generateQrCode(data, logo_path)
 
-        output_path = os.path.join(OUTPUT_DIR, "qr_code.png")
+        #Save QR Code Image
+        qr_filename = f"{uuid.uuid4()}.png"
+        output_path = os.path.join(OUTPUT_DIR, qr_filename)
         img.save(output_path)
+
+        #URL for browser 
+        qr_image_URL = f"/{output_path}"
         
-        return send_file(output_path, as_attachment = True)
     
-    return render_template("index.html")
+    return render_template("index.html", qr_image_URL = qr_image_URL)
 
 if __name__ == "__main__":
     app.run(debug = True)
